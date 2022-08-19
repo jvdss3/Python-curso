c67766a4c79c14ff55f84d21ddf6a3e30531a463
João Vitor <202108462691@alunos.unifavip.edu.br>
2022-08-19 15:10:12 -0300

    Add main
    Adicionando todas as atividades.

---
+++ Cap. 2 - Funções embutidas/01_utilfuncs_tarefa.py
@@ -0,0 +1,23 @@
+# demonstrate built-in utility functions
+
+
+def main():
+    # TODO: Use any() e all() para testar para valores boleanos
+    lista = [1, 2, 3, 4, 5, 6]
+
+    # TODO: A função any vai devolver true caso qualquer valor da lista
+    # seja verdade
+    print(any(lista))
+    # TODO: A função all vai devolver true apenas se todos valores da
+    # lista forem verdade
+    print(all(lista))
+    # TODO: As funções min e max devolvem os valores mínimo e máximo
+    # respectivamente
+    print("mínimo: ", min(lista))
+    print("máximo:", max(lista))
+    # TODO: Use sum() para somar todos os valores da lista
+    print("somatório: ", sum(lista))
+
+
+if __name__ == "__main__":
+    main()
