04419ec5a87332a657be6b36917f2add9227b19b
João Vitor <202108462691@alunos.unifavip.edu.br>
2022-08-22 07:59:52 -0300

    Update 02_enums_tarefa.py

--- Cap. 5 - Classes/02_enums_tarefa.py
+++ Cap. 5 - Classes/02_enums_tarefa.py
@@ -1,16 +1,27 @@
 # define enumerations using the Enum base class
+from enum import Enum, auto
 
 # TODO: Defina a classe Fruta que herda de Enum
-
+class Fruta(Enum):
+    UVA = 1
+    BANANA = 2
+    LARANJA = 3
+    TOMATE = 4
+    PERA = auto()
 
 def main():
-    pass
+    
     # TODO: Objetos enum possuem valores e tipos de fácil leitura
-
+    # print(Fruta.UVA)
+    # print(type(Fruta.UVA))
+    # print(repr(Fruta.UVA))
     # TODO: Objetos enum possuem propriedades "name" (nome) e
     # "value" (valor)
-
+    #print(Fruta.UVA.name, fruta.UVA.value)
     # TODO: Mostre o valor gerado automaticamente para PERA)
+    frutas = dict()
+    frutas[Fruta.BANANA] = "Casca amarela"
+    print(frutas[frutas.BANANAS])
 
     # TODO: É possível usar objetos enum como chaves
 
